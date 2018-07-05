22f4ec15616e415e0a9638dc8274c83051f9528b
Michelle Dickson <19mdickson@go.dsdmail.net>
2018-07-05 08:52:52 -0600

    built our first class component for the inputs container.

---
+++ src/components/input.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Input = (title) => {
+    return (
+        <div className="input">
+            <imput/>
+            <label>{title}</label>
+        </div>   
+    )
+}
+
+export default input
